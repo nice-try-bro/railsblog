0033713447b504d137865dd8f8e085c8e9dc35b1
Alexey Kuznetsov <innercloister@gmail.com>
2013-09-23 09:19:58 +0000

    add comments as nested resource of posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Railsblog::Application.routes.draw do
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
