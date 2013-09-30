66f1d9db7f16e1bad7d02c9ae8923f8ce53d397e
Alexey Kuznetsov <innercloister@gmail.com>
2013-09-30 16:12:42 +0000

    add test for posts#create action

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -5,4 +5,11 @@ class PostsControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
+
+  test "#create" do
+    #TODO: use generators
+    new_post = { :post => { title: 'Some Title', text: 'Some text' } }
+    post :create, new_post
+    assert_response :redirect
+  end
 end
