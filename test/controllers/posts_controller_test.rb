5f5a856f57a668d70dc3d906f7adc5fbe0e214c7
Alexey Kuznetsov <innercloister@gmail.com>
2013-09-30 16:10:13 +0000

    add test for posts#new action

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#new" do
+    get :new
+    assert_response :success
+  end
 end
