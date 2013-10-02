44ce78ef21ba63607fb61954cffd24de1a1465be
Alexey Kuznetsov <innercloister@gmail.com>
2013-10-02 19:48:54 +0000

    extract post creation to separate method in posts controller test

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,20 +1,24 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
+  #FIXME: it should be owned by object creator
+  def create_post_attributes
+    attributes_for(:post)
+  end
+
   test "#new" do
     get :new
     assert_response :success
   end
 
   test "#create" do
-    new_post = attributes_for(:post)
+    new_post = create_post_attributes
     post :create, { post: new_post }
     assert_response :redirect
   end
 
   test "#update" do
-    #TODO: move setting up parameters for post to setup
-    new_post = attributes_for(:post)
+    new_post = create_post_attributes
     post :create, { post: new_post }
     same_post = Post.find_by(:title => new_post[:title])
     same_post.title = 'Some Other Title'
