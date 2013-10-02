a210e7542538346388f4c861260865bfa02e142b
Alexey Kuznetsov <innercloister@gmail.com>
2013-10-02 10:41:19 +0000

    add test for posts#update action

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -12,4 +12,19 @@ class PostsControllerTest < ActionController::TestCase
     post :create, new_post
     assert_response :redirect
   end
+
+  test "#update" do
+    #TODO: move setting up parameters for post to setup
+    #TODO: use special tools for generating data
+    new_post = { :post => { title: 'Some Title', text: 'Some text' } }
+    post :create, new_post
+    same_post = Post.find_by(:title => 'Some Title')
+    same_post.title = 'Some Other Title'
+    put :update, :id => same_post.id, :post => same_post.attributes
+    assert_response :redirect
+    #FIXME: it uses and tests real model, which considered to be bad
+    altered_post = Post.find_by(:title => 'Some Other Title')
+    #FIXME: seems there should be one assertion per method
+    assert altered_post
+  end
 end
