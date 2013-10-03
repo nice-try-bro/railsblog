0815bb3032cb22ee0a544470e1e253d951fa6c87
Alexey Kuznetsov <innercloister@gmail.com>
2013-10-03 15:49:36 +0000

    set log_level to debug for test end development environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Railsblog::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Set log level to debug
+  config.lof_level = :debug
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ Railsblog::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Set logger level to debug
+  config.log_level = :debug
 end
