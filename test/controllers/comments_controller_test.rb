ca0c3b685e7417255c6725e34ae5e3699d22cc96
Alexey Kuznetsov <innercloister@gmail.com>
2013-10-04 07:15:05 +0000

    implement comments#create test

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#create" do
+    new_post = create :post
+    post_id = new_post.id
+    new_comment = build :comment
+    #FIXME: implement without overriding associated post
+    post :create, { :post_id => post_id, :comment => new_comment.attributes }
+    assert_response :redirect
+    saved_comment = Comment.find_by_post_id(post_id)
+    assert saved_comment
+  end
 end
