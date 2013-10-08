27d77f96b74e9c80e3bc239c789c917ca8e2d18e
Alexey Kuznetsov <innercloister@gmail.com>
2013-10-08 05:53:50 +0000

    add comment creation to posts factory

--- test/factories/posts.rb
+++ test/factories/posts.rb
@@ -1,6 +1,14 @@
 FactoryGirl.define do
   factory :post do
+    ignore do
+      comments_count 5
+    end
+
     title
     text
+
+    after(:create) do |post, evaluator|
+      FactoryGirl.create_list(:comment, evaluator.comments_count, :post => post)
+    end
   end
 end
