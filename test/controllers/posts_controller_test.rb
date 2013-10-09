4e7f891d605bc03fc63a8dffa681e4c1022963e6
Alexey Kuznetsov <innercloister@gmail.com>
2013-10-09 08:32:54 +0000

    refactor posts tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -14,21 +14,20 @@ class PostsControllerTest < ActionController::TestCase
 
   test "#create" do
     new_post = build :post
-    post_text = new_post.text
     post :create, :post => new_post.attributes
     assert_response :redirect
-    created_post = Post.find_by(:text => post_text)
-    assert_equal post_text, created_post.text
+    created_post = Post.find_by(:text => new_post.text)
+    assert_equal new_post.text, created_post.text
   end
 
   test "#update" do
     new_post = create :post
-    new_title = generate :title
-    new_post.title = new_title
+    new_text = generate :text
+    new_post.text = new_text
     put :update, :id => new_post.id, :post => new_post.attributes
     assert_response :redirect
     new_post.reload
-    assert_equal new_title, new_post.title
+    assert_equal new_text, new_post.text
   end
 
   test "#show" do
@@ -46,8 +45,10 @@ class PostsControllerTest < ActionController::TestCase
   test "#destroy" do
     new_post = create :post
     post_id = new_post.id
-    delete :destroy, :id => post_id
+    delete :destroy, :id => new_post.id
     assert_response :redirect
-    assert !Post.exists?(:id => post_id)
+    Rails.logger.debug "Deleted post: #{new_post}"
+    #NOTE: new_post object may be used instead of post_id
+    assert !Post.exists?(post_id)
   end
 end
