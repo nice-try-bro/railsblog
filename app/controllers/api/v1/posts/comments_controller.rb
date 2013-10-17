498b727979f104875143d30f54b5daf70ac96a29
Alexey Kuznetsov <innercloister@gmail.com>
2013-10-17 13:35:21 +0000

    add comments#index action in api

--- app/controllers/api/v1/posts/comments_controller.rb
+++ app/controllers/api/v1/posts/comments_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::Posts::CommentsController < Api::V1::Posts::ApplicationController
+  def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
+    respond_with(@comments)
+  end
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
