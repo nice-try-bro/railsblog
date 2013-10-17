2400a297f015e6fe0f45e4290283a3f35dfc528a
Alexey Kuznetsov <innercloister@gmail.com>
2013-10-17 09:41:05 +0000

    add comments#create test for api

---
+++ test/controllers/api/v1/posts/comments_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class Api::V1::Posts::CommentsControllerTest < ActionController::TestCase
+  test "#create" do
+    @post = create :post
+    @comment_attrs = attributes_for 'post/comment'
+    @params = { :post_id => @post.id, :format => :json }
+    post :create, @params.merge(:post_comment => @comment_attrs)
+    assert_response :created
+    @created_comment = @post.comments.find_by(:body => @comment_attrs[:body])
+    assert @created_comment
+  end
+end
