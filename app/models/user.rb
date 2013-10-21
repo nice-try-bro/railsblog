c9d4f269a63dd79f2788501c4a44307e9dee2fb2
Alexey Kuznetsov <innercloister@gmail.com>
2013-10-21 13:27:30 +0000

    add validations for post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password :validations => false
+
+  validates :name, :presence => true
+  validates :email, :uniqueness => true, :presence => true, :email => true
+  validates :login, :uniqueness => true, :presence => true
+  validates :password_digest, :presence => true
 end
